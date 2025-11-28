881d04a86ae65b194d5848d52d91000f794cdd9c
RURAR <158255885+RURAR@users.noreply.github.com>
2025-11-28 23:03:35 +0600

    liang

---
+++ liang book/chapter 2/Ex2_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex2_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a value in pounds: ");
+        double pounds = input.nextDouble();
+
+        double kilogram = pounds/0.454;
+
+        System.out.println(pounds + " Pound(s) is corresponding to " + kilogram + " kilogram(s)");
+
+
+    }
+}
